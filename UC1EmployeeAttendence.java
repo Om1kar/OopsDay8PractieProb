3607458095a83f57cee4e76f501565c9af96b244
omkar <omkardudhewar121@gmail.com>
2022-08-11 12:44:47 +0530

    Attendence

---
+++ UC1EmployeeAttendence.java
@@ -0,0 +1,18 @@
+package Oops.PracticeProb;
+
+public class UC1EmployeeAttendence {
+    public static void main(String args[]) {
+        checkEmpPresentorAbsent();
+    }
+
+    static void checkEmpPresentorAbsent() {
+        int present = 1;
+        int empCheck = (int) Math.floor(Math.random()*10)%2;
+        if (empCheck == present) {
+            System.out.println("Employee is Present");
+        }
+        else System.out.println("Employee is Absent");
+    }
+}
+
+
